0cd09083a20e14a7b2e515139d0bff55cb4ac988
Yasmine <yasmine.elkhyari@canal-plus.com>
2017-04-27 17:02:17 +0200

    liste films

--- front/src/js/EndOfGame.js
+++ front/src/js/EndOfGame.js
@@ -2,13 +2,24 @@ import React from 'react'
 import {Link, Route} from "react-router-dom";
 
 export const EndOfGame = (props) => {
-    console.log(props.history);
+
+    var rows = [];
+    props.history.map(function(answer) {
+        rows.push(
+        <div key={answer}>
+        <img src={answer[0]} style={{width: "10vw"}}/>
+        <h5 className="title">{answer[1]}</h5>
+        </div>
+        );
+    }.bind(this))
+
     return (
         <div>
             <h2>{`Score: ${props.score}`}</h2>
-            <h2><Link to="/listFilm">Liste des films</Link></h2>
+            <h2>Liste des films</h2>
+            {rows}
             <h2><Link to="/ranking">Classement</Link></h2>
             <h2><Link to="/">Retour</Link></h2>
         </div>
     );
-}
+};
