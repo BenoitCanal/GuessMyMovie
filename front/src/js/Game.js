43401e8ee97d64818f964904317789f0b8757412
aeryshev <alexey.eryshev@canal-plus.com>
2017-04-27 13:54:22 +0200

    passage sur des templates

--- front/src/js/Game.js
+++ front/src/js/Game.js
@@ -1,10 +1,27 @@
-import React from 'react'
-import {GameModeSelector} from "./GameModeSelector";
+import React from "react";
 import {Route} from "react-router-dom";
-import {ThemeSelector} from "./ThemeSelector";
 
-export const Game = () => (
-    <div>
+export class Game extends React.Component {
+    URL = "http://localhost:9000";
 
-    </div>
-)
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <img src="http://localhost:9000/assets/img/1.jpg"/>
+        );
+    }
+
+    get(URL) {
+        const params = {
+            method: "GET"
+        };
+        window.fetch(URL, params)
+    }
+
+    componentDidMount() {
+        window.fetch(URL, )
+    }
+}
