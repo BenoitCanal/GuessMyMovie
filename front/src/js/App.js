87d4cfe8a245de8a4b17ec29a9f7e3611c0837ae
Victoria Bissengué <victoria.bissengue@canal-plus.com>
2017-04-27 11:51:53 +0200

    reorganize

--- front/src/js/App.js
+++ front/src/js/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './../assets/img/logo.svg';
+import './../css/App.css';
 
 import {
     BrowserRouter as Router,
